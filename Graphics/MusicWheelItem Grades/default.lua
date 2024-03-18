98504fd337f5ec88cd83ef87355c7a47f4727c83
SugoiFactory <tony.f.cheng@gmail.com>
2024-03-17 17:58:18 -0700

    Implement quint support on songwheel

--- Graphics/MusicWheelItem Grades/default.lua
+++ Graphics/MusicWheelItem Grades/default.lua
@@ -1,6 +1,72 @@
 -- if we're in CourseMode, return a blank Actor now
 if GAMESTATE:IsCourseMode() then return NullActor end
 
+local player = nil
+local pn = nil
+
+local AwardMap = {
+	["StageAward_FullComboW1"] = 1,
+	["StageAward_FullComboW2"] = 2,
+	["StageAward_SingleDigitW2"] = 2,
+	["StageAward_OneW2"] = 2,
+	["StageAward_FullComboW3"] = 3,
+	["StageAward_SingleDigitW3"] = 3,
+	["StageAward_OneW3"] = 3,
+	["StageAward_100PercentW3"] = 3,
+	-- FullComboW4 technically doesn't exist, but we create it on the fly below.
+	["StageAward_FullComboW4"] = 4,
+}
+
+local ClearLamp = { color("#0000CC"), color("#990000") }
+
+local function GetLamp(song)
+	if player == nil then return nil end
+	if not song then return nil end
+	
+	if not GAMESTATE:GetCurrentSteps(pn) then return nil end
+	
+	local diff = GAMESTATE:GetCurrentSteps(pn):GetDifficulty()
+	
+	local stepsList = song:GetAllSteps()
+	local steps = nil
+	
+	for check in ivalues(stepsList) do
+		if check:GetDifficulty() == diff then
+			steps = check
+			break
+		end
+	end
+	
+	if steps == nil then return nil end
+	
+	local profile = PROFILEMAN:GetProfile(player)
+	local high_score_list = profile:GetHighScoreListIfExists(song, steps)
+			
+	-- If no scores then just return.
+	if high_score_list == nil or #high_score_list:GetHighScores() == 0 then
+		return nil
+	end
+
+	local best_lamp = nil
+
+	for score in ivalues(high_score_list:GetHighScores()) do
+		local award = score:GetStageAward()
+		
+		if award and AwardMap[award] ~= nil then
+			best_lamp = math.min(best_lamp and best_lamp or 999, AwardMap[award])
+		end
+		
+		if best_lamp == 1 and score:GetScore() == 0 then
+			best_lamp = 0
+		elseif best_lamp == nil then
+			if score:GetGrade() == "Grade_Failed" then best_lamp = 52
+			else best_lamp = 51 end
+		end
+	end
+
+	return best_lamp
+end
+
 
 -- how many GradeTiers are defined in Metrics.ini?
 local num_tiers = THEME:GetMetric("PlayerStageStats", "NumGradeTiersUsed")
@@ -17,24 +83,52 @@ end
 -- assign the "Grade_Failed" key a value equal to num_tiers
 grades["Grade_Failed"] = num_tiers
 
+return Def.ActorFrame{
+	Def.Sprite{
+		Name="Grades",
+		Texture=THEME:GetPathG("MusicWheelItem","Grades/grades 1x18.png"),
+		InitCommand=function(self) self:zoom( SL_WideScale(0.18, 0.3) ):animate(false) end,
 
-return Def.Sprite{
-	Texture=THEME:GetPathG("MusicWheelItem","Grades/grades 1x18.png"),
-	InitCommand=function(self) self:zoom( SL_WideScale(0.18, 0.3) ):animate(false) end,
+		-- "SetGrade" is broadcast by the engine in MusicWheelItem.cpp.
+		-- It will be passed a table with, at minimum, one parameter:
+		--     PlayerNumber (PlayerNumber enum as string)
+		--
+	   -- and potentially two more if the current song/course and steps/trail have a non-null HighScoreList
+		--     Grade (GradeTier as number)
+		--     NumTimesPlayed (number)
+		SetGradeCommand=function(self, params)
+			if not (params.Grade and grades[params.Grade]) then
+				self:visible(false)
+				return
+			end
+			
+			if params.PlayerNumber ~= nil then
+				player=params.PlayerNumber
+				pn=ToEnumShortString(params.PlayerNumber)
+			end
 
-	-- "SetGrade" is broadcast by the engine in MusicWheelItem.cpp.
-	-- It will be passed a table with, at minimum, one parameter:
-	--     PlayerNumber (PlayerNumber enum as string)
-	--
-   -- and potentially two more if the current song/course and steps/trail have a non-null HighScoreList
-	--     Grade (GradeTier as number)
-	--     NumTimesPlayed (number)
-	SetGradeCommand=function(self, params)
-		if not (params.Grade and grades[params.Grade]) then
-			self:visible(false)
-			return
+			self:setstate(grades[params.Grade])
+		end,
+		
+		
+	},
+	
+	Def.Sprite{
+		Name="Quint",
+		Texture=THEME:GetPathG("MusicWheelItem","Grades/quint.png"),
+		InitCommand=function(self) self:zoom( SL_WideScale(0.18, 0.3) ):animate(false):visible(false) end,
+		SetCommand=function(self, params)
+			if not params.Song then return end
+			if pn == nil then return end
+			
+			local lamp = GetLamp(params.Song)
+			if lamp == 0 then
+				self:visible(true)
+				self:GetParent():GetChild("Grades"):visible(false)
+			else
+				self:visible(false)
+				if lamp ~= nil then self:GetParent():GetChild("Grades"):visible(true) end
+			end
 		end
-
-		self:visible(true):setstate(grades[params.Grade])
-	end
+	},
 }
