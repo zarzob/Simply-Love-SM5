d80432e634cfd31ec7098123164b4762076f45af
zarzob <73825889+zarzob@users.noreply.github.com>
2024-04-26 23:28:29 +1200

    initial commit

--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -58,6 +58,146 @@ end
 -- ensure that af2 is added as a child of af
 af[#af+1] = af2
 
+-- Tutorial stuff
+-- Create rounded quads 
+
+
+af[#af+1] = Def.ActorFrame {
+
+	-- Background
+	Def.ActorMultiVertex {
+		OnCommand=function(self)
+			self:SetDrawState({Mode="DrawMode_Fan"})
+			local width = 202
+			local height = 202
+			local color = Color.White
+			local intensity = 0.1
+			self:SetVertices(createRoundedQuad(width,height, color, intensity,false,true,true,false))
+		end
+	},
+	-- Background border
+	Def.ActorMultiVertex {
+		OnCommand=function(self)
+			self:SetDrawState({Mode="DrawMode_Fan"})
+			local width = 200
+			local height = 200
+			local color = Color.Red
+			local intensity = 0.1
+			self:SetVertices(createRoundedQuad(width,height, color, intensity,false,true,true,false))
+		end
+	},
+
+	-- Tab 1
+	Def.ActorFrame {
+		-- Tab 1 background
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 42
+				local height = 10
+				local color = Color.White
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(-160,-210)
+			end
+		},
+		-- Inside
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 40
+				local height = 8
+				local color = Color.Blue
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(-160,-210)
+			end
+		},
+	},
+	-- Tab 2
+	Def.ActorFrame {
+		-- Background
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 42
+				local height = 10
+				local color = Color.White
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(-78,-210)
+			end
+		},
+		-- Inside
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 40
+				local height = 8
+				local color = color("#808080")
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(-78,-210)
+			end
+		},
+	},
+	-- Tab 3
+	Def.ActorFrame {
+		-- Background
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 42
+				local height = 10
+				local color = Color.White
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(4,-210)
+			end
+		},
+		-- Inside
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 40
+				local height = 8
+				local color = color("#808080")
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(4,-210)
+			end
+		},
+	},
+	-- Tab 4
+	Def.ActorFrame {
+		-- Background
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 42
+				local height = 10
+				local color = Color.White
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(86,-210)
+			end
+		},
+		-- Inside
+		Def.ActorMultiVertex {
+			OnCommand=function(self)
+				self:SetDrawState({Mode="DrawMode_Fan"})
+				local width = 40
+				local height = 8
+				local color = color("#808080")
+				local intensity = 0.5
+				self:SetVertices(createRoundedQuad(width,height, color, intensity,true,false,false,true))
+				self:xy(86,-210)
+			end
+		},
+	},
+	
+
+}
 -- -----------------------------------------------------------------------
 
 return af
