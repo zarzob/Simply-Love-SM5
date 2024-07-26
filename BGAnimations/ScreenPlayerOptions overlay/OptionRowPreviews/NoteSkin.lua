2382b4dc3438e033f5afb12355df6ef84582de2b
SugoiFactory <tony.f.cheng@gmail.com>
2024-07-25 20:17:37 -0700

    Cooler noteskin preview in player options!

--- BGAnimations/ScreenPlayerOptions overlay/OptionRowPreviews/NoteSkin.lua
+++ BGAnimations/ScreenPlayerOptions overlay/OptionRowPreviews/NoteSkin.lua
@@ -3,5 +3,15 @@ local t = ...
 -- Add noteskin actors to the primary AF and hide them immediately.
 -- We'll refer to these later via ActorProxy in ./Graphics/OptionRow Frame.lua
 for noteskin in ivalues( CustomOptionRow("NoteSkin").Choices ) do
-	t[#t+1] = LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin})
+	local arrows = Def.ActorFrame{
+		Name="NoteSkin_"..noteskin,
+		InitCommand=function(self)
+			self:visible(false)
+		end
+	}
+	t[#t+1] = arrows
+	arrows[#arrows+1] = LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin,column="Left",offset=-96,quant=0})
+	arrows[#arrows+1] = LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin,column="Down",offset=-32,quant=1})
+	arrows[#arrows+1] = LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin,column="Up",offset=32,quant=3})
+	arrows[#arrows+1] = LoadActor(THEME:GetPathB("","_modules/NoteSkinPreview.lua"), {noteskin_name=noteskin,column="Right",offset=96,quant=2})
 end
