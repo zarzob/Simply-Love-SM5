3c9aac9acbb798e5d11fe24e8615917f9a68e674
SugoiFactory <tony.f.cheng@gmail.com>
2024-07-30 10:29:09 -0700

    Fix layout loading due to error bar changes

--- Scripts/SL-Layout.lua
+++ Scripts/SL-Layout.lua
@@ -23,8 +23,17 @@ function GetGameplayLayout(player, reverse)
 
     local topY = judgmentY - judgmentHeight/2
     local bottomY = judgmentY + judgmentHeight/2
+	
+	local hasErrorBar = false
+	local ErrorBarTypes = { "Colorful", "Monochrome", "Text", "Highlight", "Average" }
+	for i, barname in ipairs(ErrorBarTypes) do
+		if mods[barname] then 
+			hasErrorBar = true
+			break
+		end
+	end
 
-    if mods.ErrorBar ~= "None" then
+    if hasErrorBar then
         if mods.JudgmentGraphic == "None" then
             -- Display the error bar in place of the judgment graphic if it's
             -- disabled.
