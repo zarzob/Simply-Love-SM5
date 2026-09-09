9ed16dc44558924baf307419fdd7f4c5ac3a8cd7
Symmetrical <108992124+Symmetricalifinity@users.noreply.github.com>
2026-09-09 02:09:45 -0400

    Section count reverted to Miso-only...
    This had to be done because sorting songs in any way other than by group would result in incorrect numbers (mainly zero) and there was no way that I knew of to get the actual section count from the metrics.
    
    For one, trying to use LoadActor in the metrics just... won't display any additional actor definitions on-screen, even if I end the command with a return.
    
    I also tried using AddWrapperState, but that would result in many more of them being added as I changed music wheel selections, thus making it inefficient.
    
    The only thing that kind of worked was using AddChildFromPath in conjunction with AddWrapperState, but even this wouldn't work that well since the actor can't exactly get parameters from the original element.
    
    It pained me to do this since I wanted the music wheel to maintain a consistent yet customizable font, but I just wasn't able to find an efficient way to change it at the moment, yet I still wanted the information shown to be correct...

--- Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
+++ Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
@@ -34,20 +34,6 @@ local af = Def.ActorFrame{
 			DiffuseEmojis(self)
 		end,
 	},
-	Def.BitmapText {
-		Font=ThemePrefs.Get("ThemeFont") .. " Normal",
-		InitCommand=function(self)
-			self:halign(1):xy(_screen.w/2 - WideScale(37, 43),0):zoom(0.75)
-		end,
-		SetCommand=function(self, params)
-			if (not params.Song or params.Course) then
-				local count = #SONGMAN:GetSongsInGroup(params.Text) or 0
-				self:settext((ThemePrefs.Get("nice")>0 and count==69) and "nice" or count)
-			else
-				self:settext("")
-			end
-		end,
-	},
 	Def.ActorFrame{
 		Name="FolderStack",
 		InitCommand=function(self)
